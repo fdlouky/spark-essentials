23c61fa088c55e3f0963d874d3e7b3d5cc1ae777
fdlouky <fmdlouky@gmail.com>
2022-07-29 17:57:11 +0200

    Work on Datasets.scala for Datasets part 2 and solve some exercises

--- src/main/scala/part3typesdatasets/Datasets.scala
+++ src/main/scala/part3typesdatasets/Datasets.scala
@@ -1,8 +1,7 @@
 package part3typesdatasets
 
-import org.apache.spark.sql.functions.{avg, col, sum, to_date}
-import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Encoder, Encoders, SparkSession}
+import org.apache.spark.sql.functions.{array_contains, avg, col, sum, to_date}
+import org.apache.spark.sql._
 
 import java.sql.Date
 
@@ -52,7 +51,9 @@ object Datasets extends App {
 
 
   // 3 - define an encoder (importing the implicits)
+
   import spark.implicits._
+
   // 4 - convert the DF to DS
   val carsDS = carsWithFabDateDF.as[Car]
 
@@ -81,12 +82,40 @@ object Datasets extends App {
   println(carsDS.map(_.Horsepower.getOrElse(0.0)).reduce(_ + _) / carsCount) // 103.5295566502463
 
   // also use the DF Functions!
-  carsDS.select(avg(col("Horsepower"))).show // 105.0825
+  carsDS.select(avg(col("Horsepower"))) // 105.0825
 
   // why result from line 84 is different of line 81?
   println(carsDS.map(_.Horsepower.getOrElse(0.0)).reduce(_ + _)) // 42033.0
-  carsDS.select(sum(col("Horsepower"))).show // 42033
+  carsDS.select(sum(col("Horsepower"))) // 42033
   // 42033/105.0825 = 400 -> there are 6 rows with "Horsepower":null
   // so avg here is not considering rows with null values
+
+  // Joins
+  case class Guitar(id: Long, make: String, model: String, guitarType: String)
+  case class GuitarPlayer(id: Long, name: String, guitars: Seq[Long], band: Long)
+  case class Band(id: Long, name: String, hometown: String, year: Long)
+
+  val guitarsDS = readDF("guitars.json").as[Guitar]
+  val guitarPlayersDS = readDF("guitarPlayers.json").as[GuitarPlayer]
+  val bandsDS = readDF("bands.json").as[Band]
+
+  val guitarPlayerBandsDS: Dataset[(GuitarPlayer, Band)] = guitarPlayersDS
+    .joinWith(bandsDS, guitarPlayersDS.col("band") === bandsDS.col("id"), "inner")
+  //  guitarPlayerBandsDS.show() -> a Dataset of tuple of two Dataset
+
+  /*
+    Exercise: join the guitarsDS and guitarPlayersDS, in an outer join
+    (hint: use array_contains)
+   */
+
+  val guitarPlayersGuitarsDS = guitarPlayersDS
+    .joinWith(guitarsDS, array_contains(guitarPlayersDS.col("guitars"), guitarsDS.col("id")), "outer")
+
+  // Grouping DS
+  val carsGroupedByOrigin = carsDS
+    .groupByKey(_.Origin)
+    .count()
+
+  // joins and groups are WIDE transformations, will involve SHUFFLE operations
 
 }
